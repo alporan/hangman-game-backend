3cd15a78278ac4b10840f223ba553b6f1b5b33cb
alporan <alporan@windowslive.com>
2020-08-08 13:02:39 +0300

    Initial Commit

---
+++ src/test/java/com/alporan/hangmangame/HangmanGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alporan.hangmangame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
